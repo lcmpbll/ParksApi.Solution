f40c62285096d124c99daf3d952f11d40aba8ac5
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-19 09:51:24 -0700

    Add class to line 7 to properly use Context

--- ParksApi/Models/ParksApiContext.cs
+++ ParksApi/Models/ParksApiContext.cs
@@ -4,12 +4,13 @@ namespace ParksApi.Models
 {
   public class ParksApiContext : DbContext
   {
-    public class ParksApiContext(DbContextOptions<ParksApiContext> options)
-      :base(options)
-      {
-        
+    public ParksApiContext(DbContextOptions<ParksApiContext> options)
+      : base(options)
+      { 
       }
       
       public DbSet<Park> Parks { get; set; }
   }
 }
+
+

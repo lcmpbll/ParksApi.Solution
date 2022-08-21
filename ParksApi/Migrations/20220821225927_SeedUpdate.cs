85eed361a8448978f1f1d3b0f2cd522839c632b8
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-21 16:20:16 -0700

    Update seed data

---
+++ ParksApi/Migrations/20220821225927_SeedUpdate.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class SeedUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5,
+                column: "ParkMgmt",
+                value: "National Park");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6,
+                column: "ParkMgmt",
+                value: "National Park");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 11,
+                column: "ParkMgmt",
+                value: "National Park");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5,
+                column: "ParkMgmt",
+                value: "National Parks");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6,
+                column: "ParkMgmt",
+                value: "National Parks");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 11,
+                column: "ParkMgmt",
+                value: "National Parks");
+        }
+    }
+}
